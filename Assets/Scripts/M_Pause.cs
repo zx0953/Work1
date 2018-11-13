8b76fcc84d26e5245eb6a1da9017f284d8bd014f
zx0953 <lovesky0953zx@gmail.com>
2018-11-13 20:36:07 +0800

    first commit

---
+++ Assets/Scripts/M_Pause.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class M_Pause : MonoBehaviour {
+    AudioSource m_MyAudioSource;
+    public Button BtnPP;
+
+    //Play the music
+    bool m_Play;
+
+    void Start()
+    {
+        BtnPP.onClick.AddListener(BtnPPOnclick);
+        //Fetch the AudioSource from the GameObject
+        m_MyAudioSource = GetComponent<AudioSource>();
+        //Ensure the toggle is set to true for the music to play at start-up
+        m_Play = true;
+    }
+
+    void Update()
+    {
+        try
+        {
+            if (m_Play == true)
+            {
+                m_MyAudioSource.UnPause();
+            }
+            else
+            {
+                m_MyAudioSource.Pause();
+            }
+        }catch(Exception exp)
+        {
+            Debug.LogError(exp.ToString());
+        }
+    }
+
+    void OnGUI()
+    {
+       
+    }
+    void BtnPPOnclick() {
+        Debug.Log("BtnMMOnClick");
+        if (m_Play == true)
+        {
+            m_Play = false;
+        }
+        else
+        {
+            m_Play = true;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjectHandle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHandle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 vec3 = this.transform.eulerAngles;
+        vec3.y += 20 * Time.deltaTime;
+        vec3.y %= 360;
+     /*   vec3.x += 20 * Time.deltaTime;
+        vec3.x %= 90;
+        vec3.z += 20 * Time.deltaTime;
+        vec3.z %= 360; */
+        this.transform.eulerAngles = vec3;
+	}
+}
